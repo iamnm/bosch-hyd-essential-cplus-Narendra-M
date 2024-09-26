0f8b5e7c30f21d5ba561826c183d59c87a349216
iamnm <narendramengaji88@gmail.com>
2024-09-26 09:51:05 +0530

    Create InheritanceComposition .cpp

---
+++ InheritanceComposition .cpp
@@ -0,0 +1,66 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Device{
+  protected:
+    string assetId,model;
+  public:
+    Device(string  assetIdArg,string modelArg): assetId{assetIdArg},model{modelArg}{
+        cout<<"Device Instantiated"<<endl;
+    }
+  
+    ~Device(){
+        cout<<"Device Destructed"<<endl;
+    }
+};
+class Printer:public Device{
+  public:
+    Printer(string  assetIdArg,string modelArg):Device::Device(assetIdArg,modelArg){
+        cout<<"Printer Instantiated"<<endl;
+    }
+    void print(string content){ cout<<"Printer Printing "<<content<<endl;
+    }
+    ~Printer(){
+        cout<<"Printer Destructed"<<endl;
+    }
+};
+
+class Scanner:public Device{
+ 
+  public:
+    Scanner(string  assetIdArg,string modelArg): Device::Device(assetIdArg,modelArg){
+        cout<<"Scanner Instantiated"<<endl;
+    }
+    void scan(string content){ cout<<"Scanner Scanning "<<content<<endl;
+    }
+    ~Scanner(){
+        cout<<"Scanner Destructed"<<endl;
+    }
+};
+
+
+class PrintScanner{
+    public:
+    PrintScanner(string assetId,string model){
+        Printer PrinterObj("P1", "HP");
+        Scanner ScannerObj("S1", "HP");
+        cout<<"PrintScanner Instantiated"<<endl;
+    }
+    
+    string getModelName(){
+    //    return this->model;
+    return "";
+    }
+    ~PrintScanner(){
+        cout<<"Print Scanner Destructed"<<endl;
+    }
+};
+
+
+int main() {
+   PrintScanner psObj{"PS1","HP"};
+
+    return 0;
+}
